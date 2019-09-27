14a2ac212e2cf905b5bce98d5189fd1badd21110
Vlad Glavtchev <vlad.glavtchev@gmail.com>
2019-09-27 13:33:56 -0700

    More mobile optimizations.

--- src/App.js
+++ src/App.js
@@ -25,37 +25,74 @@ export class App extends Component {
   };
   
   render() {
+    const { width } = this.state;
+    const isMobile = width <= 800;
+
+    let countDownDate = new Date("Oct 4, 2019 0:00:00").getTime();
+    let now = this.state.time;
+
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
+    let isBirthday = true;// (countDownDate - now) < 0;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
     // Countdown component
     const Countdown = () => {
+      const boldStyle = (
+        isMobile ? (
+          {fontSize: '2rem'}
+        ) : (
+          {fontSize: '3rem'}
+        )
+      );
+
+      const regularStyle = (
+        isMobile ? (
+          {fontSize: '1rem'}
+        ) : (
+          {fontSize: '2rem'}
+        )
+      );
+
       return (
         <>
-        <p className="fg-text-bold">
+        <p className="fg-text-bold" style={boldStyle}>
           MARTY turns 6 in...
         </p>
-        <p></p>
+        <p style={regularStyle}>
           {days} days 
-        <p></p>
+        </p>
+        <p style={regularStyle}>
           {hours} hours 
-        <p></p>
+        </p>
+        <p style={regularStyle}>
           {minutes} minutes
-        <p></p>
+        </p>
+        <p style={regularStyle}>
           {seconds} seconds
+        </p>
         </>)
       }
 
     // Congrats component
     const Congrats = ({time}) => {
+      const textSize = isMobile ? '2rem' : '3rem';
       if ( (time / 1000).toFixed(0) % 2 == 0) {
         return (
           <>
-            <p className="fg-text-bold" style={{color: 'yellow'}}>
+            <p className="fg-text-bold" style={{color: 'yellow', fontSize: textSize}}>
             HAPPY BIRTHDAY, MARTY!!!
             </p>
           </>)
       } else {
         return (
           <>
-            <p className="fg-text-bold" style={{color: 'white'}}>
+            <p className="fg-text-bold" style={{color: 'white', fontSize: textSize}}>
             HAPPY BIRTHDAY, MARTY!!!
             </p>
           </>)
@@ -63,7 +100,7 @@ export class App extends Component {
     }
 
     // Desktop component
-    const DesktopLayout = ({isBirthday}) => {
+    const DesktopLayout = () => {
       return (
         <>
           <div className="column">
@@ -85,7 +122,7 @@ export class App extends Component {
     }
 
     // Mobile component
-    const MobileLayout = ({isBirthday}) => {
+    const MobileLayout = () => {
       return (
         <>
           <div className="column">
@@ -103,30 +140,12 @@ export class App extends Component {
       )
     }
 
-    const { width } = this.state;
-    const isMobile = width <= 800;
-
-    let countDownDate = new Date("Oct 4, 2019 0:00:00").getTime();
-    let now = this.state.time;
-
-    // Find the distance between now and the count down date
-    let distance = countDownDate - now;
-    let isBirthday = (countDownDate - now) < 0;
-
-    // Time calculations for days, hours, minutes and seconds
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    let content;
-  
-
     return (
       <div className="module">
         <div className="centered">
           <div className="row">
             {
-              isMobile ? (<MobileLayout isBirthday={isBirthday}></MobileLayout>) : (<DesktopLayout isBirthday={isBirthday}></DesktopLayout>)
+              isMobile ? (<MobileLayout></MobileLayout>) : (<DesktopLayout></DesktopLayout>)
             }
           </div>
         </div>
